9ae4a33b05008b4ee20ecca7aa0f3e35fd3caae3
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-10-21 23:06:34 -0700

    adds logic to get times and showtime formats

--- main.go
+++ main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"fmt"
+	"encoding/json"
+	"strings"
 	"log"
 	"net/http"
-	_"io/ioutil"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
-func main(){
+func main() {
 	//Get request to HTML page
 	url := "https://www.cinemark.com/theatres/wa-bellevue/cinemark-lincoln-square-cinemas-and-imax"
 	res, err := http.Get(url)
 
 	if err != nil {
-		log.Println("Failed fetching data from website:",err)
+		log.Println("Failed fetching data from website:", err)
 	}
 
 	defer res.Body.Close()
@@ -24,15 +25,6 @@ func main(){
 		log.Printf("Status code error: %s", res.Status)
 	}
 
-	// Read the response body
-    // body, err := ioutil.ReadAll(res.Body)
-    // if err != nil {
-    //     fmt.Println("Error reading response body:", err)
-    //     return
-    // }
-    // htmlContent := string(body)
-    // fmt.Println(htmlContent)
-
 	//Load the HTML doc
 	doc, err := goquery.NewDocumentFromReader(res.Body)
 
@@ -40,9 +32,28 @@ func main(){
 		log.Fatal("Error loading the document:", err)
 	}
 
+	movies := make(map[string]map[string][]string)
+
 	doc.Find("div.showtimeMovieBlock").Each(func(i int, s *goquery.Selection) {
-		fmt.Printf("Review %d: %s\n", i, s.Find("a.movieLink > h3").Text());
+		title := s.Find("a.movieLink > h3").Text()
+
+		var formats []string
+		s.Find(".print-type-list").Each(func(j int, f *goquery.Selection) {
+			formats = append(formats, strings.TrimSpace(f.Text()))
+		})
+
+		movies[title] = make(map[string][]string)
+		s.Find(".showtimeMovieTimes").Each(func(j int, f *goquery.Selection) {
+			f.Find("a.showtime-link").Each(func(k int, t *goquery.Selection) {
+				movies[title][formats[j]] = append(movies[title][formats[j]], t.Text())
+			})
+		})
 	})
-	
+
+	b, err := json.MarshalIndent(movies, "", "  ")
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Print(string(b))
 
 }
