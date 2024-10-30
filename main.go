16210dac7fed751ea7ec54952a5fad597b7b31a9
Sai Ramya Linga <sairamyalinga@gmail.com>
2024-10-30 13:04:43 -0700

    change in css tag for formats and movie details function

--- main.go
+++ main.go
@@ -1,14 +1,32 @@
 package main
 
 import (
-	"fmt"
 	"encoding/json"
-	"strings"
+	"fmt"
 	"log"
 	"net/http"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+func getPriceDetails(url string) string {
+	details, err := http.Get(url)
+	if err != nil {
+		log.Println("Failed to fetch price and seat arrangement:", err)
+	}
+	defer details.Body.Close()
+	if details.StatusCode != 200 {
+		log.Printf("Status code error: %s", details.Status)
+	}
+	doc, err := goquery.NewDocumentFromReader(details.Body)
+	if err != nil {
+		log.Fatal("Error loading the Seating details document:", err)
+	}
+	price := doc.Find("div.ticketSelectorHeader > h3").Text()
+	fmt.Println(price);
+	return price
+
+}
 
 func main() {
 	//Get request to HTML page
@@ -35,20 +53,40 @@ func main() {
 	movies := make(map[string]map[string][]string)
 
 	doc.Find("div.showtimeMovieBlock").Each(func(i int, s *goquery.Selection) {
+		//get movie title
 		title := s.Find("a.movieLink > h3").Text()
 		var formats []string
-		s.Find(".print-type-list").Each(func(j int, f *goquery.Selection) {
-			formats = append(formats, strings.TrimSpace(f.Text()))
-		})
-		movies[title] = make(map[string][]string)
-		s.Find(".showtimeMovieTimes").Each(func(j int, f *goquery.Selection) {
-			f.Find("a.showtime-link").Each(func(k int, t *goquery.Selection) {
-				seatingURL, exists := t.Attr("href")
+
+		//extract movie formats
+		s.Find(".attribute-list__item").Each(func(j int, f *goquery.Selection) {
+			aTag := f.Find("a")
+			if aTag.Length() > 0 {
+				dataId, exists := aTag.Attr("data-print-type")
 				if exists{
-				fmt.Print("exists")
-				movies[title][formats[j]] = append(movies[title][formats[j]], t.Text(), seatingURL)
+					formats = append(formats, dataId)
+				}
+			} else {
+				formats = append(formats, f.Text())
 			}
+
+		})
+		fmt.Println(formats)
+		movies[title] = make(map[string][]string)
+		x := 0
+		s.Find(".showtimeMovieTimes").Each(func(j int, f *goquery.Selection) {
+			movieTag := f.Find("a.showtime-link")
+			if movieTag.Length() > 0{
+			f.Find("a.showtime-link").Each(func(k int, t *goquery.Selection) {
+				fmt.Println("Movie and format:", title, formats[x])
+				seatingURL, exists := t.Attr("href")
+				if exists {
+					movies[title][formats[x]] = append(movies[title][formats[x]], t.Text(), seatingURL)
+					fmt.Println(x, t.Text())
+				}
 			})
+			x = x + 1;
+		}     
+			
 		})
 	})
 
